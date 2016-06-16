4ada4b2e037ca3af2d0c3699bfa7b433dabd5557
Emilio López <emilio@elopez.com.ar>
2016-06-15 21:33:57 -0300

    First version of a script to install dotfiles

---
+++ setup.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+set -e
+
+# This setup.sh script requires:
+#  * git
+#  * sh
+
+ROOT=$(cd $(dirname $0); git rev-parse --show-toplevel)
+
+# To support dotfiles referencing other scripts and files
+# without needing to copy them, we need a standard path
+# they can reference. Let's standarize on "~/.dotfiles"
+if [ "$ROOT" != "$HOME/.dotfiles" ]; then
+	echo "ERROR: you must clone the repo as ~/.dotfiles/" >&2
+	exit 1
+fi
+
+# Prepare backup directory
+BACKUPS="$ROOT/backups/$(date +%s)/"
+mkdir -p "$BACKUPS"
+
+cd "$ROOT"
+for module in *; do
+	# not a module - skip
+	if [ ! -d "$module" ]; then
+		continue;
+	fi
+
+	# incomplete module - skip
+	if [ ! -f "$module/dotfiles" ]; then
+		continue;
+	fi
+
+	echo "Installing dotfiles for $module"
+
+	# install dotfiles - back up existing ones
+	for dotfile in $(cat "$module/dotfiles"); do
+		MODULE="$ROOT/$module"
+		ORIG="$MODULE/$(echo $dotfile | cut -f1 -d:)"
+		DEST="$HOME/$(echo $dotfile | cut -f2 -d:)"
+
+		# if destination is unset, use default
+		if [ "$DEST" = "$HOME/" ]; then
+			DEST="$HOME/$ORIG"
+		fi
+
+		# if dotfile already exists, back it up
+		if [ -f "$DEST" ]; then
+			mv "$DEST" "$BACKUPS/"
+		fi
+
+		# link the dotfile
+		ln -vs "$ORIG" "$DEST"
+	done		
+done
